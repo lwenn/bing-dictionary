0c8a9ec07ef467867ec63f875259204ee3bd9bf4
lwenn <lwenn@foxmail.com>
2018-06-13 23:38:38 +0800

    feat: 修改弹框样式；恢复弹框内链接跳转

--- main.js
+++ main.js
@@ -26,14 +26,19 @@
     let handle = {
         container: null,
         target: null,
+        clickContainer: false,
 
         init: function() {
-            this.initEvent();
             this.initContainer();
+            this.initEvent();
         },
 
         initEvent: function() {
+            var self = this;
             document.addEventListener('mouseup', this.searchWords.bind(this));
+            this.container.addEventListener('mouseup', function() {
+                self.clickContainer = true;
+            }, true);
         },
 
         initContainer: function() {
@@ -43,11 +48,16 @@
         },
 
         searchWords: function (e) {
+            // 点击词典内不查询
+            if (this.clickContainer) {
+                this.clickContainer = false;
+                return;
+            }
             let se = this;
             let words = window.getSelection ? window.getSelection().toString() : document.selection.createRange().text;
             let xhr = new XMLHttpRequest();
             se.target = e.target;
-            console.log(e.clientX, e.clientY);
+            // console.log(e.clientX, e.clientY);
             if (words) {
                 xhr.open('get', '//cn.bing.com/dict/search?q=' + words);
                 // cros 跨域
@@ -63,14 +73,30 @@
                     if (xhr.status === 200) {
                         let result = xhr.responseText;
                         // console.log(result);
-                        // console.log(se.parseHtml(result));
-                        let res = result.match(/<body.*?>([\s\S]*)<\/body>/);
-                        let html = res[1].replace(/(<script[\s\S]*?\/script>|<style[\s\S]*?\/style>|<link[\s\S]*?>|<img[\s\S]*?>)/g, '');
-                        // console.log(html);
-                        se.showTranslation(e, html);
+                        se.showTranslation(e, se.getContentElement(result));
                     }
                 }
             }
+        },
+
+        getContentElement: function(result) {
+            // console.log(se.parseHtml(result));
+            let res = result.match(/<body.*?>([\s\S]*)<\/body>/);
+            // 过滤外部资源
+            let html = res[1].replace(/(<script[\s\S]*?\/script>|<style[\s\S]*?\/style>|<link[\s\S]*?>|<img[\s\S]*?>)/g, '');
+            // console.log(html);
+            this.container.innerHTML = html;
+            let content = this.container.querySelector('.content .qdef');
+            if (content) {
+                let links = content.querySelectorAll('a');
+                for (let i = 0; i < links.length; i++) {
+                    if (links[i].href.indexOf('javascript:') === -1) {
+                        links[i].target = '_blank';
+                        links[i].href = '//cn.bing.com' + links[i].getAttribute('href');
+                    }
+                }
+            }
+            return content;
         },
 
         parseHtml: function (html) {
@@ -145,26 +171,28 @@
             return html.substring(targetStart, targetEnd);
         },
 
-        showTranslation: function (e, html) {
-            this.container.innerHTML = html;
-            let content = this.container.querySelector('.content .qdef');
-
-            if (!content) {
+        showTranslation: function (e, element) {
+            if (!element) {
                 return;
             }
-            content.style = `width: ${WIDTH}px; max-height: ${HEIGHT}px`;
+            let div = document.createElement('div');
+            div.style = `width: ${WIDTH}px; max-height: ${HEIGHT}px`;
+            div.appendChild(element);
             this.container.innerHTML = '';
-            this.container.appendChild(content);
+            this.container.appendChild(div);
 
             // let domRect = this.target.getBoundingClientRect();
             // let posLeft = domRect.left + pageXOffset - WIDTH / 2 + this.target.offsetWidth / 2;
             // this.container.style.top = domRect.top + pageYOffset + this.target.offsetHeight + 'px';
-            let posTop = e.clientY + pageYOffset + 15;
-            let posLeft = e.clientX + pageXOffset - WIDTH / 2;
-            posLeft = posLeft < 0 ? 0 : (posLeft > document.body.scrollWidth - WIDTH ? document.body.scrollWidth - WIDTH : posLeft);
+            this.container.style.display = 'block';
+
+            let conWdith = this.container.offsetWidth;
+            let posTop = e.clientY + pageYOffset + 25;
+            let posLeft = e.clientX + pageXOffset - conWdith / 2;
+            posLeft = posLeft < 0 ? 0 : (posLeft > document.body.scrollWidth - conWdith ? document.body.scrollWidth - conWdith : posLeft);
+
             this.container.style.top = posTop + 'px';
             this.container.style.left = posLeft + 'px';
-            this.container.style.display = 'block';
             this.container.scrollTo(0, 0);
         },
 
